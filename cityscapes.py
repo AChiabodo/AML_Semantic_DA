a7c751d5df45e3536a8ba6dbb52e4a7a68af2ee0
alessandro <alessandro.chiabodo@gmail.com>
2023-12-19 19:53:50 +0100

    update (cityscapes) - extracted transformations to util

--- cityscapes.py
+++ cityscapes.py
@@ -4,15 +4,13 @@ from collections import namedtuple
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 from PIL import Image
-import torchvision.transforms.functional as F
-from torchvision.datasets.utils import extract_archive, iterable_to_str, verify_str_arg
+from utils import ExtResize, ExtToTensor, ExtTransforms , ExtCompose
+from torchvision.datasets.utils import iterable_to_str, verify_str_arg
 from torchvision.datasets.vision import VisionDataset
-from torchvision import transforms
 import numpy as np
-import torch
+
 class CityScapes(VisionDataset):
 
-    # Based on https://github.com/mcordts/cityscapesScripts
     CityscapesClass = namedtuple(
         "CityscapesClass",
         ["name", "id", "train_id", "category", "category_id", "has_instances", "ignore_in_eval", "color"],
@@ -69,7 +67,7 @@ class CityScapes(VisionDataset):
         target_type: Union[List[str], str] = "semantic",
         transform: Optional[Callable] = None,
         target_transform: Optional[Callable] = None,
-        transforms: Optional[Callable] = None,
+        transforms: Optional[ExtTransforms] = None,
     ) -> None:
         super().__init__(root, transforms, transform, target_transform)
         print("root: ", root)
@@ -124,36 +122,14 @@ class CityScapes(VisionDataset):
     def __getitem__(self, index: int) -> Tuple[Any, Any]:
 
         image = Image.open(self.images[index]).convert("RGB")
-
-        targets: Any = []
-        for i, t in enumerate(self.target_type):
-            if t == "polygon":
-                target = self._load_json(self.targets[index][i])
-            else:
-                # Keep the target in grayscale, as it's typically a label map
-                target = Image.open(self.targets[index][i])
-            targets.append(target)
-        target = targets[0]
-
-        #transform = transforms.Compose([transforms.ToTensor()])
-        #image = transform(image)
-            # target = torch.from_numpy( np.array( F.resize(target, (512,1024), Image.NEAREST, antialias=True), dtype='uint8') ) #
-        image , target = ExtResize((512,1024))(image,target)
-        image , target = ExtToTensor()(image,target)
-        #target = torch.from_numpy( np.array( target, dtype='uint8') )
+        target = Image.open(self.targets[index][0])
+        image , target = self.transforms(image,target)
+        #image , target = ExtResize((512,1024))(image,target)
+        #image , target = ExtToTensor()(image,target)
         return image, target
 
     def __len__(self) -> int:
         return len(self.images)
-
-    def extra_repr(self) -> str:
-        lines = ["Split: {split}", "Mode: {mode}", "Type: {target_type}"]
-        return "\n".join(lines).format(**self.__dict__)
-
-    def _load_json(self, path: str) -> Dict[str, Any]:
-        with open(path) as file:
-            data = json.load(file)
-        return data
 
     def _get_target_suffix(self, mode: str, target_type: str) -> str:
         if target_type == "instance":
@@ -174,54 +150,3 @@ class CityScapes(VisionDataset):
         colorized_preds = Image.fromarray(colorized_preds[0]) # to PIL Image
         colorized_labels = Image.fromarray(colorized_labels[0])
         return colorized_preds , colorized_labels
-
-
-class ExtResize(object):
-    """Resize the input PIL Image to the given size.
-    Args:
-        size (sequence or int): Desired output size. If size is a sequence like
-            (h, w), output size will be matched to this. If size is an int,
-            smaller edge of the image will be matched to this number.
-            i.e, if height > width, then image will be rescaled to
-            (size * height / width, size)
-        interpolation (int, optional): Desired interpolation. Default is
-            ``PIL.Image.BILINEAR``
-    """
-
-    def __init__(self, size, interpolation=Image.BILINEAR):
-        self.size = size
-        self.interpolation = interpolation
-
-    def __call__(self, img, lbl):
-        """
-        Args:
-            img (PIL Image): Image to be scaled.
-        Returns:
-            PIL Image: Rescaled image.
-        """
-        return F.resize(img, self.size, self.interpolation), F.resize(lbl, self.size, Image.NEAREST)
-    
-class ExtToTensor(object):
-    """Convert a ``PIL Image`` or ``numpy.ndarray`` to tensor.
-    Converts a PIL Image or numpy.ndarray (H x W x C) in the range
-    [0, 255] to a torch.FloatTensor of shape (C x H x W) in the range [0.0, 1.0].
-    """
-    def __init__(self, normalize=True, target_type='uint8'):
-        self.normalize = normalize
-        self.target_type = target_type
-    def __call__(self, pic, lbl):
-        """
-        Note that labels will not be normalized to [0, 1].
-        Args:
-            pic (PIL Image or numpy.ndarray): Image to be converted to tensor.
-            lbl (PIL Image or numpy.ndarray): Label to be converted to tensor. 
-        Returns:
-            Tensor: Converted image and label
-        """
-        if self.normalize:
-            return F.to_tensor(pic), torch.from_numpy( np.array( lbl, dtype=self.target_type) )
-        else:
-            return torch.from_numpy( np.array( pic, dtype=np.float32).transpose(2, 0, 1) ), torch.from_numpy( np.array( lbl, dtype=self.target_type) )
-
-    def __repr__(self):
-        return self.__class__.__name__ + '()'
